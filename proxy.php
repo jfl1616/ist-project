1ebc8dbc9d700b59b2923e0af03375984702597d
Jose Lopez Jr <jfl1616@rit.edu>
2026-03-19 00:22:39 -0700

    update proxy.php file

--- proxy.php
+++ proxy.php
@@ -1,34 +1,4 @@
 <?php
-<<<<<<< HEAD
-//supress warnings & errors
-error_reporting(0);
-
-//sets the base of the url we want to hit
-define ('HOSTNAME', 'http://www.ist.rit.edu/api');
-
-//access the api - remember we have to send in a leading '/'
-//so the path variable could be '/about/'
-$url=HOSTNAME.$_GET['path'];
-
-//set up curl (Client Uniform Resource Locator)
-	//  Initiate curl
-	$ch = curl_init();
-	// allow us to include a header in the return (we are setting to false as we don't need to)
-	curl_setopt($session, CURLOPT_HEADER, false);
-	// Will return the response, if false it print the response (we want to capture it in a variable $result)
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	// Set the url
-	curl_setopt($ch, CURLOPT_URL,$url);
-	// Execute
-	$result=curl_exec($ch);
-	// Closing
-	curl_close($ch);
-
-//we want json back so set the correct mimetype
-header("Content-Type: text/plain");
-
-//give it to me!
-=======
 // Turn ON error reporting for debugging (turn off in production)
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -85,6 +55,5 @@ curl_close($ch);
 
 // Output as JSON
 header('Content-Type: application/json');
->>>>>>> feature/docker-setup
 echo $result;
 ?>
